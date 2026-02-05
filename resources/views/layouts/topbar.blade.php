696e848b321d6718c2ece5a1130cbdb075a9d2ca
jinten anggraeni <jintenanggraeni6@gmail.com>
2026-02-05 12:20:32 +0700

    update topbar

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -1,19 +1,15 @@
-<nav id="topNavbar" class="navbar navbar-expand-lg bg-white border-bottom px-4" style="box-shadow: 0 2px 8px rgb(37 99 235 / 0.15); border-bottom: 2px solid #2563eb;">
-  <div class="d-flex align-items-center justify-content-between w-100">
+<nav id="topNavbar" class="navbar navbar-expand-lg bg-white border-bottom px-4" style="box-shadow: 0 2px 8px rgb(37 99 235 / 0.15); border-bottom: 2px solid #2563eb; position:fixed; top:0; left:0px; right:0; z-index:999; padding:0.5rem 1rem; background-color:#ffffff;">
+  <div class="d-flex align-items-center justify-content-between w-100" style="position:fixed; top:0; left:0px; right:0; z-index:999; padding:0.5rem 1rem; background-color:#ffffff;">
 
     <!-- Hamburger kiri -->
     <button class="btn btn-light" id="sidebarToggle" aria-label="Toggle sidebar">
       <i class="bi bi-list fs-4"></i>
     </button>
 
-   
-
     <!-- Logo kanan -->
-    <div class="navbar-brand fw-bold fs-4 text-primary ms-auto d-flex align-items-center gap-2">
+    <div class="navbar-brand fw-bold fs-4 text-primary ms-auto d-flex align-items-center gap-2" >
       <i class="bi bi-house-fill"></i>
       Rumah Cloteh
     </div>
-
   </div>
 </nav>
-
